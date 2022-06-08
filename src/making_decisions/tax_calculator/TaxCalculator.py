fcbd2393864d579e7ac355219e5199c6c122be63
Jake Walker <jakewalk93@hotmail.com>
2022-06-08 18:56:39 +0100

    Refactored tests for tax calculator

--- src/making_decisions/tax_calculator/TaxCalculator.py
+++ src/making_decisions/tax_calculator/TaxCalculator.py
@@ -1,31 +1,52 @@
 from decimal import Decimal
 
-
 class TaxCalculator:
     def __init__(self):
-        self.order_amount = 0
-        self.state = ''
+        self.__order_amount = 0
+        self.__state = ""
+        self.__TAX_PERCENTAGE = 0.055
         self.wisconsin_values = ["WI", "Wi",
                                  "wi", "wI", "Wisconsin", "wisconsin"]
-        self.TAX_PERCENTAGE = 0.055
-        self.total = 0
+        self.__total = 0
         self.nl = '\n'
-
-    def read_input(self):
-        self.order_amount = input("Please enter an order amount: ")
-        self.state = input("Please enter an US State: ")
-
-    def compute_total(self):
-        if self.state in self.wisconsin_values:
-            self.total = float(self.order_amount) + \
-                (float(self.order_amount) * self.TAX_PERCENTAGE)
+        
+    def run(self):
+        self.read_user_input()
+        self.compute_tax_on_order()
+        
+        self.display_total()
+        
+    def read_user_input(self):
+        self.__order_amount = input("Please enter an order amount:")
+        self.__state = input("Please enter a US State:")
+        
+    def compute_tax_on_order(self):
+        if self.__state in self.wisconsin_values:
+            self.__total = float(self.__order_amount) + (float(self.__order_amount) * self.__TAX_PERCENTAGE)
             return
-        self.total = self.order_amount
+        
+        self.__total = float(self.__order_amount)
 
     def display_total(self):
-        if self.state in self.wisconsin_values:
+        if self.__state in self.wisconsin_values:
             print(
-                f"The subtotal is £{self.order_amount} {self.nl} The tax amount is £{float(self.order_amount) * self.TAX_PERCENTAGE} {self.nl} The total is £{self.total}")
-        if self.state not in self.wisconsin_values:
+                f"The subtotal is £{self.__order_amount} {self.nl} The tax amount is £{float(self.__order_amount) * self.__TAX_PERCENTAGE} {self.nl} The total is £{self.__total}")
+        if self.__state not in self.wisconsin_values:
             print(
-                f"The subtotal is £{self.order_amount} {self.nl} The total is £{self.total}")
+                f"The subtotal is £{self.__order_amount} {self.nl} The total is £{self.__total}")
+
+    @property
+    def order_amount(self):
+        return self.__order_amount
+    
+    @property
+    def state(self):
+        return self.__state
+    
+    @property
+    def tax_percentage(self):
+        return self.__TAX_PERCENTAGE
+    
+    @property
+    def total(self):
+        return self.__total
--- tests/making_decisions/tax_calculator/challenge/test_tax_calculator.py
+++ tests/making_decisions/tax_calculator/challenge/test_tax_calculator.py
@@ -1,6 +1,29 @@
 from src.making_decisions.tax_calculator.TaxCalculator import TaxCalculator
+from unittest.mock import patch
 import unittest
 
 
-def test_TaxCalculator():
-    assert 1 == 1
+class Test_TaxCalculator(unittest.TestCase):
+    
+    @patch('builtins.input')
+    def test_tax_calculator_run(self, mock_input):
+        mock_input.side_effect = [50, 'NY']
+        
+        tax_calculator = TaxCalculator()
+        tax_calculator.run()
+        
+        assert tax_calculator.total == 50
+        assert tax_calculator.state == "NY"
+        assert tax_calculator.tax_percentage == 0.055
+        
+    @patch('builtins.input')
+    def test_tax_calculator_run_state_with_wisconsin(self, mock_input):
+        mock_input.side_effect = [50, 'WI']
+        
+        tax_calculator = TaxCalculator()
+        tax_calculator.run()
+        
+        assert tax_calculator.total == 52.75
+        assert tax_calculator.order_amount == 50
+        assert tax_calculator.state == "WI"
+        assert tax_calculator.tax_percentage == 0.055
