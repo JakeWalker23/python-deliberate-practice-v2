b639504e41b6c358c9a7f6f65f176759d770b5a6
Jake Walker <jakewalk93@hotmail.com>
2022-06-17 12:18:33 +0100

    improved BMI tests

--- tests/making_decisions/bmi_calculator/test_bmi_calculator.py
+++ tests/making_decisions/bmi_calculator/test_bmi_calculator.py
@@ -3,25 +3,14 @@ from unittest.mock import patch
 import unittest
 
 class TestBMICalculator(unittest.TestCase):
-    
-    @patch('builtins.input')    
-    def test_bmi_calculate(self, mock_input):
-        mock_input.side_effect = [100, 200]
-    
-        bmi = BMI()
-        bmi.run()
-
-        assert bmi.height == 100
-        assert bmi.weight == 200
-
-
     @patch('builtins.input')   
     def test_display_bmi_is_ideal(self, mock_input):
         mock_input.side_effect = [72, 182]
     
         bmi = BMI()
         bmi.run()
-
+        assert bmi.height == 72
+        assert bmi.weight == 182
         assert bmi.bmi_index == 24.68
         
     @patch('builtins.input')   
@@ -31,6 +20,8 @@ class TestBMICalculator(unittest.TestCase):
         bmi = BMI()
         bmi.run()
 
+        assert bmi.height == 50
+        assert bmi.weight == 100
         assert bmi.bmi_index == 28.12
         
     @patch('builtins.input')   
@@ -40,6 +31,8 @@ class TestBMICalculator(unittest.TestCase):
         bmi = BMI()
         bmi.run()
 
+        assert bmi.height == 50 
+        assert bmi.weight == 40
         assert bmi.bmi_index == 11.25
         assert bmi.upper_bmi_range == 25
         assert bmi.lower_bmi_range == 18.5
