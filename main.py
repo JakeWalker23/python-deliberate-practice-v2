85bcb6864f34b76205e78a530ae1fc73b15e5a3c
Jake Walker <jakewalk93@hotmail.com>
2022-06-17 12:49:28 +0100

    Refactored legal driving age into class with private variables. Added mocked unit tests

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from src.making_decisions.bmi_calculator.BMI import BMI
+from src.making_decisions.legal_driving_age.LegalDrivingAge import DrivingAge
 
-bmi = BMI()
-bmi.run()
+driving_age = DrivingAge()
+
+driving_age.read_driving_age()
+driving_age.compare_driving_age()
+driving_age.filter_legal_countries()
+driving_age.display()
 
 
 
